377482a0a3e505b32d9505d017492af136a67f44
Adam Mikulis <adam.mikulis@gmail.com>
2023-01-26 19:21:08 -0700

    initial commit
    some UI done, localization displayed

---
+++ src/controller/LoginScreen.java
@@ -0,0 +1,31 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class LoginScreen implements Initializable {
+
+    public TextField UserNameTextField;
+    public TextField PasswordTextField;
+    public Label ZoneIdLabel;
+    public Label ErrorMessageLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("client db gui initialized");
+        Locale currentLocale = Locale.getDefault();
+        ZoneIdLabel.setText("Current locale: " + currentLocale);
+        ErrorMessageLabel.setText("");
+
+    }
+
+    public void onLoginButtonAction(ActionEvent actionEvent) {
+        System.out.println("User not found!");
+    }
+}
